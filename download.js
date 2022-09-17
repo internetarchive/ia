639bb4c0957b27c7e21d0fe01f9f49a78749b2a1
Tracey Jaquith <tracey@archive.org>
2022-09-17 22:58:51 +0000

    decent progress per file downloading sort of working.  need to get each file into own async/promise backgrounded state for *true* progress -- but all the parts otherwise are there and working now

--- download.js
+++ download.js
@@ -2,23 +2,59 @@ import $ from 'https://esm.archive.org/jquery'
 import './download/demo.js'
 import log from './util/log.js'
 
-const msg = (txt, id) => {
-  const e = document.createElement('div')
-  if (id) e.setAttribute('id', id)
-  e.classList = 'card card-body bg-light'
-  e.innerHTML = txt
-  $('#msgs').prepend(e)
-}
+window.$ = $
 
 let NFILES = 0
+const FINISHED_FADE_TIME = 2500 // 2.5s
+const FILEKEYS = {}
+let DIRH
 
-async function download_item() {
+const msg = (IAID, file) => {
+  const txt = `downloading: [${IAID}] ${file}`
+  const msgid = `msg${NFILES}`
+  const e = document.createElement('div')
+  e.setAttribute('id', msgid)
+  e.classList = 'card card-body bg-light'
+  e.innerHTML = `
+  ${txt}
+  <div class="progress">
+    <div class="progress-bar progress-bar-striped progress-bar-animated bg-info" role="progressbar" style="width: 0%" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">&nbsp;</div>
+  </div>`
+  $('#msgs').prepend(e)
+  NFILES += 1
+  return msgid
+}
+
+async function progress_update() {
+  for (const [filekey, info] of Object.entries(FILEKEYS)) {
+    const { msgid, size, size_last } = info
+    const parts = filekey.split('/')
+    const IAID = parts[0]
+    const outfile = parts.slice(1).join('/')
+    const subdir = await DIRH.getDirectoryHandle(IAID)
+    const fh = await subdir.getFileHandle(outfile)
+    const filedata = await fh.getFile()
+    const size_now = filedata.size
+
+    if (size_last !== size_now) {
+      FILEKEYS[filekey].size_last = size_now
+      const percent = Math.round(100 * (size_now / size))
+      $(`#${msgid} .progress-bar`).css('width', `${percent}%`)
+    }
+  }
+  setTimeout(progress_update, 750)
+}
+
+async function download_items() {
   const IAIDS = document.getElementById('IAIDS').value.split(',')
 
-  const dir_handle = await window.showDirectoryPicker({
+  DIRH = await window.showDirectoryPicker({
     // startIn: 'desktop',
     mode: 'readwrite',
   })
+
+
+  setTimeout(progress_update, 1000)
 
   for (const IAIDin of IAIDS) {
     const IAID = IAIDin.trim()
@@ -30,10 +66,12 @@ async function download_item() {
     // const prefix = `https://${mdapi.d1}${mdapi.dir}/${IAID}/`
 
     for (const fileobj of mdapi.files.sort()) {
-      NFILES += 1
       const file = fileobj.name
-      msg(`downloading: [${IAID}] ${file}`, `msg${NFILES}`)
-
+      const msgid = msg(IAID, file)
+      const $msgdiv = $(`#${msgid}`)
+      const outfile = file.replace(/.*\//, '') // like basename() xxx preserve optional item subdirs
+      const filekey = `${IAID}/${outfile}`
+      FILEKEYS[filekey] = { msgid, size: file.size, size_last: 0 }
       try {
         const data = await fetch(`${prefix}${file}`)
 
@@ -41,11 +79,8 @@ async function download_item() {
         // but a demo start
         const blob = await data.blob()
 
-        const outfile = file.replace(/.*\//, '') // like basename()
-        log('WRITE TO', `${IAID}/${outfile}`)
-
         // create IDENTIFIER name subdirectory
-        const subdir = await dir_handle.getDirectoryHandle(IAID, { create: true })
+        const subdir = await DIRH.getDirectoryHandle(IAID, { create: true })
 
         // xxx if file exists and is expected size, skip
         const fh = await subdir.getFileHandle(outfile, { create: true })
@@ -54,14 +89,18 @@ async function download_item() {
         const writable = await fh.createWritable()
         await writable.write(blob)
         await writable.close()
-        $(`#msg${NFILES}`).addClass('alert alert-success').fadeOut(2500)
+        $msgdiv.find('.progress-bar').css('width', '100%')
+        $msgdiv.addClass('alert alert-success').fadeOut(FINISHED_FADE_TIME)
       } catch (error) {
         log({ error }) // xxx skip over CORS-restricted files for now
-        $(`#msg${NFILES}`).addClass('alert alert-danger').fadeOut(2500)
+        $msgdiv.find('.progress-bar').css('width', '100%')
+        $msgdiv.addClass('alert alert-danger').fadeOut(FINISHED_FADE_TIME)
       }
+      delete FILEKEYS[filekey]
+      setTimeout(() => $msgdiv.remove(), FINISHED_FADE_TIME)
     }
   }
 }
 
 
-document.getElementById('download-item').addEventListener('click', download_item)
+document.getElementById('download-item').addEventListener('click', download_items)
