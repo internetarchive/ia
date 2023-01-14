14571186c2acbca876b65a681029444af0385ab7
Promaethius <bryant.jonathan.42@gmail.com>
2023-01-13 20:39:06 -0700

    92.1% lcov

--- example/main.js
+++ example/main.js
@@ -1,9 +1,27 @@
 /* eslint-disable no-console */
-import Search from './search.js'
-import Fields from './fields.js'
-import * as Query from './query.js'
-import * as Queue from './queue.js'
-import Result from './result.js'
+import Search from '../lib/search.js'
+import Fields from '../lib/fields.js'
+import File from '../lib/file.js'
+import * as Query from '../lib/query.js'
+import * as Queue from '../lib/queue.js'
+import Result from '../lib/result.js'
+
+let testresults = []
+testresults = []
+testresults.push(
+  new Result('twitter-894946694244204545'),
+)
+
+const testqueue = new Queue.DownloadQueue(10, 5, async (file, stream, md5) => {
+  console.log(file)
+  console.log(file instanceof File)
+  console.log(stream, ReadableStream)
+  console.log(await md5)
+}, ...testresults)
+await testqueue.download({
+  '894946694244204545.jpg': 5,
+})
+console.log(testqueue.downloadErrors())
 
 const any = new Query.QueryString('any', 'dragon')
 const media = new Query.QueryMediaType('etree')
